09f42a514e34d28d6d67dc7db3bb7c997bc08b0e
Natas21 <65798632+Natas21@users.noreply.github.com>
2022-09-23 10:55:26 -0700

    Update matrices.py

--- matrices.py
+++ matrices.py
@@ -1,11 +1,11 @@
 import os
-import readchar
-
-c1=1
-c2=1
+opciones_mat = ["+","-","*"]
 class matrices:
-    def __init__(self,longitud):
-        self.largo = longitud
+    def __init__(self,filas1,filas2,columnas1,columnas2):
+        self.filas1 = filas1
+        self.filas2 = filas2
+        self.columnas1 = columnas1
+        self.columnas2 = columnas2
         self.matriz1 = []
         self.matriz2 = []
         self.matriz3 = []
@@ -19,27 +19,48 @@ class matrices:
         if len(self.matriz1) == len(self.matriz2):
             for i in range(len(self.matriz1)):
                 self.matriz3.append((self.matriz1[i] + self.matriz2[i]))
-            print(self.matriz3)
             return self.matriz3
         else:
             error = print("No se puede realizar la suma porque no son de la misma longitud")
             return error
-
-filas1 = int(input("Filas que tendra su matriz: "))
-columnas1 = int(input("Columnas que tendra su matriz: "))
-longitud = filas1 * columnas1
-resultado = matrices(longitud)
-while c1 <= longitud:
-    c1 += 1
-    m1 = resultado.agregar1(n1= float(input("Valor que desea agregar: ")))
-filas2 = int(input("Filas que tendra su matriz: "))
-columnas2 = int(input("Columnas que tendra su matriz: "))
-longitud = filas2*columnas2
-resultado2 = matrices(longitud)
-while c2 <= longitud:
-    c2 +=1
-    m2 = resultado.agregar2(n2=float(input("Valor que desea agregar: ")))
-longitud3 = longitud
-resultado3 = matrices(longitud3)
-m3 = resultado.Sum_M()
-print(m1,m2,m3)
+    def Res_M(self):
+        if len(self.matriz1) == len(self.matriz2):
+            for i in range(len(self.matriz1)):
+                self.matriz3.append((self.matriz1[i] - self.matriz2[i]))
+            return self.matriz3
+        else:
+            error = print("No se puede realizar la resta porque no son de la misma longitud")
+            return error
+    def Mult_M(self):
+        if self.filas1 != self.columnas2:
+            error = "Las filas de la matriz 1 y las columnas de la matriz 2 deben de ser iguales para realizar la operacion"
+            return error
+        else:
+            for i in range (self.filas1):
+                for j in range (self.columnas2):
+                    self.matriz3.append(((self.matriz1[i])*(self.matriz2[j]))+(self.matriz1[j])*(self.matriz2[i]))
+            return self.matriz3
+def menu_mat ():
+    c1 = 1
+    c2 = 1
+    resultado = matrices(filas1 = int(input("Filas que tendra su matriz #1: ")), columnas1 = int(input("Columnas que tendra su matriz#1: ")), filas2 = int(input("Filas de matriz #2: ")), columnas2 = int(input("Columnas de matriz #2: ")))
+    longitud = resultado.filas1*resultado.filas2
+    while c1 <= longitud:
+        c1 += 1
+        m1 = resultado.agregar1(n1=float(input("Valor que desea agregar a matriz 1: ")))
+    while c2 <= longitud:
+        c2 += 1
+        m2 = resultado.agregar2(n2=float(input("Valor que desea agregar a matriz 2: ")))
+    opm = input("Operacion a realizar: ")
+    if opm not in opciones_mat:
+        print("Por favor elija una opcion valida ")
+    elif opm == "+":
+        m3 = resultado.Sum_M()
+        print(m1," + ",m2," = ",m3)
+    elif opm == "-":
+        m3 = resultado.Res_M()
+        print(m1," - ",m2," = ",m3)
+    elif opm == "*":
+        m3 = resultado.Mult_M()
+        print(m1, " * ", m2, " = ", m3)
+menu_mat()
